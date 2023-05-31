1b6bf3d5572bcb03f98c24dbd37b6be66be9e909
mehmet çoban <74604824+mcban34@users.noreply.github.com>
2023-05-31 11:00:55 +0300

    detailPageContent

--- product-detail.js
+++ product-detail.js
@@ -5,16 +5,13 @@ fetch("public/product.json")
     const urlParams = new URLSearchParams(window.location.search);
     const productId = urlParams.get("id");
 
+    let dataObject = [] 
     let gelenVeri = value.find(item => item.id == productId);
-
+    console.log(gelenVeri);
+    dataObject.push(gelenVeri)
 
     const slides = gelenVeri.detailImg.map((slide) => `<div><img class='zoomable-image' src="${slide.detailImg}" alt=""></div>`).join("");
     const navSlides = gelenVeri.detailImg.map((slide, index) => `<div><a href="#" data-slide="${index + 1}"><img src='${slide.detailImg}'></a></div>`).join("");
-
-    console.log(slides);
-    console.log(navSlides)
-
-
 
     const productHTML = `
     <div class="slider slider-for">
@@ -24,9 +21,36 @@ fetch("public/product.json")
         <div class="slider slider-nav">
             ${navSlides}
         </div>
-    </div>`;
+    </div>`
 
     document.querySelector(".productImg").innerHTML = productHTML;
+
+
+    let detailContentHTML = dataObject.map(element => {
+        
+            //!yıldızlar
+            let ratingStars = [];
+            for (let j = 0; j < parseInt(element.rating); j++) {
+                ratingStars.push('<i class="bi bi-star-fill"></i>');
+            }
+            for (let j = 0; j < 5 - parseInt(element.rating); j++) {
+                ratingStars.push('<i class="bi bi-star"></i>');
+            }
+            ratingHtml = ratingStars.join('');
+        return `
+
+            <div class="detailContent">
+                <h1 class="detailTitle">${element.title}</h1>
+                <p class="detailDes">${element.description}</p>
+                <div class="rating-stars">${ratingHtml} <span class="ratingHtml">(${element.rating.toFixed(1)})</span>  <span class="ticked">${element.ticket}</span></div>
+                <h2>${element.price.newPrice}₺ <span class="oldPrice">${element.price.oldprice}₺</span> </h2>
+                <button class="addToCard">Sepete Ekle</button>
+            </div>
+
+        `
+    })
+    document.querySelector(".productDetailContent").innerHTML=detailContentHTML.join("")
+
 
     $(document).ready(function(){
         $('.slider-for').slick({
