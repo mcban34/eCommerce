f5b5701d0c412a30493cd80134cc92aff8499dc5
mehmet çoban <74604824+mcban34@users.noreply.github.com>
2023-06-04 22:30:46 +0300

    slider content

--- app.js
+++ app.js
@@ -280,6 +280,7 @@ $(document).ready(function(){
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 3500
+      autoplaySpeed: 3500,
+      nav:false
     });
 });
