d5f9e228dfc9e1345eda155e07a732a1d93377ab
mehmet çoban <74604824+mcban34@users.noreply.github.com>
2023-06-05 03:08:55 +0300

    slider end commit

--- app.js
+++ app.js
@@ -280,7 +280,7 @@ $(document).ready(function(){
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 3500,
+      autoplaySpeed: 2500,
       nav:false,
       dots:true
     });
